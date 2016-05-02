8d2ed3b599759c6b47fcb76fc0a105d02ce99222
PrzemekX <przemek7574@gmail.com>
2016-05-02 19:28:53 +0200

    -Dodanie widgetów dla plot1
    -Dodanie widgetu Checkbox group dla plot1
    -Dodanie widgetu Slider Range dla plot1

--- server.R
+++ server.R
@@ -5,6 +5,9 @@
 # http://shiny.rstudio.com
 #
 
+#install.packages("xlsx")
+#install.packages("ggplot2")
+
 library(shiny)
 library(xlsx) # to read excel files
 library(ggplot2) # to plot
@@ -15,27 +18,59 @@ dataFromExcel <- read.xlsx(file="Szwajcaria Dane.xlsx", sheetIndex=1,header=TRUE
 shinyServer(function(input, output) {
   options(scipen=999)
   df <- dataFromExcel
-  df$Date <- as.Date(as.character(df$Date), format="%Y-%m-%d")
   
   #######FIRST PLOT#######
+  df$Date <- as.Date(as.character(df$Date), format="%Y-%m-%d")
   output$Plot1 <- renderPlot({
-     x <- df$Date # first column with Date
-     y <- df[ , 2:length(df)] # (all columns from df without the first one, the first column was x = Date)
+    
+    #####WIDGET - SLIDER RANGE - OPEN#####
+    y1 <- input$Plot_1_2[1] - 2002
+    y2 <- (-1) * (2003 - input$Plot_1_2[2])
+    df2 <- df[y1:y2, ]
+    #####WIDGET - SLIDER RANGE - END#####
+    
+    #####WIDGET - OPTION - OPEN#####
+    InputMod <- input$Plot_1_1
+    for(i in 1:6) {
+      InputMod[length(InputMod)+1] <- 0
+    }
+    gl1 <- NULL
+    gl2 <- NULL
+    gl3 <- NULL
+    gl4 <- NULL
+    gl5 <- NULL
+    gl6 <- NULL
+    
+    if(InputMod[1] == 1) {
+      gl1 <- geom_line(data = df2, aes(x = x, y = y$Swisscom_prepaid, color=" Swisscom_prepaid "), linetype = 2, size = 1.6)
+    }
+    if(InputMod[1] == 2 || InputMod[2] == 2) {
+      gl2 <- geom_line(data = df2, aes(x = x, y = y$Swisscom_postpaid, color=" Swisscom_postpaid "), linetype = 2, size = 1.6)
+    }
+    if((InputMod[1] == 3 || InputMod[2] == 3) || InputMod[3] == 3) {
+      gl3 <- geom_line(data = df2, aes(x = x, y = y$Sunrise_prepaid, color=" Sunrise_prepaid "), linetype = 2, size = 1.6)
+    }
+    if((((InputMod[1] == 4 || InputMod[2] == 4) || InputMod[3] == 4) || ( InputMod[4] == 4))) {
+      gl4 <- geom_line(data = df2, aes(x = x, y = y$Sunrise_postpaid, color=" Sunrise_postpaid "), linetype = 2, size = 1.6)
+    }
+    if(((InputMod[1] == 5 || InputMod[2] == 5) || (InputMod[3] == 5 || InputMod[4] == 5)) || InputMod[5] == 5) {
+      gl5 <- geom_line(data = df2, aes(x = x, y = y$Orange_prepaid, color=" Orange_prepaid "), linetype = 2, size = 1.6)
+    }
+    if(((((InputMod[1] == 6 || InputMod[2] == 6) || InputMod[3] == 6) || InputMod[4] == 6) || InputMod[5] == 6) || InputMod[6] == 6) {
+     gl6 <- geom_line(data = df2, aes(x = x, y = y$Orange_postpaid, color=" Orange_postpaid "), linetype = 2, size = 1.6)
+    }
+    #####WIDGET - OPTION - END#####
+
+     x <- df2$Date # first column with Date
+     y <- df2[ , 2:length(df2)] # (all columns from df without the first one, the first column was x = Date)
      plotGgplot <- ggplot() +
-      geom_line(data = df, aes(x = x, y = y$Swisscom_prepaid, color=" Swisscom_prepaid "), linetype = 2, size = 1.6) +
-      geom_line(data = df, aes(x = x, y = y$Swisscom_postpaid, color=" Swisscom_postpaid "), linetype = 2, size = 1.6) +
-      geom_line(data = df, aes(x = x, y = y$Sunrise_prepaid, color=" Sunrise_prepaid "), linetype = 2, size = 1.6) +
-      geom_line(data = df, aes(x = x, y = y$Sunrise_postpaid, color=" Sunrise_postpaid "), linetype = 2, size = 1.6) +
-      geom_line(data = df, aes(x = x, y = y$Orange_prepaid, color=" Orange_prepaid "), linetype = 2, size = 1.6) +
-      geom_line(data = df, aes(x = x, y = y$Orange_postpaid, color=" Orange_postpaid "), linetype = 2, size = 1.6) +
+      gl1 + gl2 + gl3 + gl4 + gl5 + gl6 +
       ylab('Number of Subscribers') +
       xlab('Year') +
-      # scale_x_continuous(labels = comma) +
+      # scale_x_continuous(labels = comma, breaks = seq(from=2003,to=2015,by=1)) +
       scale_y_continuous ( labels = comma, breaks = seq(from=0,to=6000000,by=200000)) +
       ggtitle("Subscribers Market Share in Switzerland for Mobile Prepaid and Postpaid market and its' competition in 2010-2015") +
-      theme(plot.title=element_text(size=8, face="bold",
-                                    hjust = 0.5),
-            axis.title=element_text(size=8))
+      theme(plot.title=element_text(size=8, face="bold", hjust = 0.5), axis.title=element_text(size=8))
      plotGgplot
   })
   
--- ui.R
+++ ui.R
@@ -13,25 +13,37 @@ shinyUI(fluidPage(
   titlePanel("Subscribers Market Share in Switzerland for Mobile Prepaid and Postpaid market and its' competition in 2010-2015"),
 
   sidebarLayout(
+    #####WIDGET - Checkbox Group - OPEN#####
     sidebarPanel(
-      #helpText("Test1"),
-      
-      #selectInput("var", 
-                 #label = "Choose a variable to display",
-                  #choices = c("Percent White", "Percent Black",
-                  #            "Percent Hispanic", "Percent Asian"),
-                  #selected = "Percent White"),
-      
-      # sliderInput("range", 
-      #             label = "Range of interest:",
-      #             min = 0, max = 100, value = c(0, 100))
-       ),
-    
+      checkboxGroupInput("Plot_1_1", label = h3("Plot 1 options - Mobile market"), 
+                         choices = list("Swisscom prepaid" = 1, "Swisscom postpaid" = 2, "Sunrise prepaid" = 3, "Sunrise postpaid" = 4, "Orange prepaid" = 5, "Orange postpaid" = 6),
+                         selected = c(1,2,3,4,5,6))
+    ),
+    #####WIDGET - Checkbox Group - END#####
     
     # Show a plot of the generated distribution
     mainPanel(
-      plotOutput("Plot1"),
-      plotOutput("Plot2")
+      plotOutput("Plot1")
+      # plotOutput("Plot2")
     )
-  )
+  ),
+  #####WIDGET - SLIDER RANGE - OPEN#####
+  fluidRow(
+    column(4,
+           
+           # Copy the line below to make a slider range 
+           sliderInput("Plot_1_2", label = h3("Year Range"), min = 2003, 
+                       max = 2015, value = c(2003, 2015))
+    )
+  ),
+  
+  hr(),
+  
+  fluidRow(
+    column(4, verbatimTextOutput("range"))
+  ),
+  #####WIDGET - SLIDER RANGE - END#####
+  
+  plotOutput("Plot2")
+  
 ))
